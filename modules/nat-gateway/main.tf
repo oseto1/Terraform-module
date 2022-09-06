63324ae968b844807da153bff78c72315047d2fa
abc <abc@example.com>
2022-09-05 21:14:58 -0400

    updated nat

--- modules/nat-gateway/main.tf
+++ modules/nat-gateway/main.tf
@@ -22,7 +22,7 @@ resource "aws_nat_gateway" "nat_gateway_az1" {
   subnet_id     = var.public_subnet_az1_id
 
   tags   = {
-    Name = "nat gateway in public subnet az1"
+    Name = "nat gateway az1"
   }
 
   # to ensure proper ordering, it is recommended to add an explicit dependency
@@ -35,7 +35,7 @@ resource "aws_nat_gateway" "nat_gateway_az2" {
   subnet_id     = var.public_subnet_az2_id
 
   tags   = {
-    Name = "nat gateway in public subnet az2"
+    Name = "nat gateway az2"
   }
 
   # to ensure proper ordering, it is recommended to add an explicit dependency
@@ -59,38 +59,38 @@ resource "aws_route_table" "private_route_table_az1" {
 
 # associate private app subnet az1 with private route table az1
 resource "aws_route_table_association" "private_app_subnet_az1_route_table_az1_association" {
-  subnet_id         = 
-  route_table_id    = 
+  subnet_id         = var.private_app_subnet_az1_id
+  route_table_id    = aws_route_table.private_route_table_az1.id
 }
 
 # associate private data subnet az1 with private route table az1
 resource "aws_route_table_association" "private_data_subnet_az1_route_table_az1_association" {
-  subnet_id         = 
-  route_table_id    = 
+  subnet_id         = var.private_data_subnet_az1_id
+  route_table_id    = aws_route_table.private_route_table_az1.id
 }
 
 # create private route table az2 and add route through nat gateway az2
 resource "aws_route_table" "private_route_table_az2" {
-  vpc_id            = 
+  vpc_id            = var.vpc_id
 
   route {
-    cidr_block      = 
-    nat_gateway_id  = 
+    cidr_block      = "0.0.0.0/0"
+    nat_gateway_id  = aws_nat_gateway.nat_gateway_az2.id
   }
 
   tags   = {
-    Name = 
+    Name = "private route table az2"
   }
 }
 
 # associate private app subnet az2 with private route table az2
 resource "aws_route_table_association" "private_app_subnet_az2_route_table_az2_association" {
-  subnet_id         = 
-  route_table_id    = 
+  subnet_id         = var.private_app_subnet_az2_id
+  route_table_id    = aws_route_table.private_route_table_az2.id
 }
 
 # associate private data subnet az2 with private route table az2
 resource "aws_route_table_association" "private_data_subnet_az2_route_table_az2_association" {
-  subnet_id         = 
-  route_table_id    = 
+  subnet_id         = var.private_data_subnet_az2_id
+  route_table_id    = aws_route_table.private_route_table_az2.id
 }
--- modules/nat-gateway/variables.tf
+++ modules/nat-gateway/variables.tf
@@ -1,4 +1,8 @@
-variable "public_subnet_az1" {}
+variable "public_subnet_az1_id" {}
 variable "internet_gateway" {}
 variable "var.public_subnet_az2" {}
 variable "vpc_id" {}
+variable "private_app_subnet_az1_id" {}
+variable "private_data_subnet_az1_id" {}
+variable "private_app_subnet_az2_id" {}
+variable "private_data_subnet_az2_id" {}
